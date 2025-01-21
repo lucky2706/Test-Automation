8f9f03246a6b3285de83441a83464a3fb6880add
lakshmanan.harikrish <lakshmanan.harikrish@SLTB233>
2025-01-21 21:18:13 +0530

    Login Script

---
+++ Test Suites/Test Suite.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>7d0a2f02-caff-493d-889f-ef0e1dc1a17a</testSuiteGuid>
+   <testCaseLink>
+      <guid>981b0433-775e-4c94-9e6a-f8f413784df9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>c51f9675-a99a-4655-9345-b00bda1cd4b4</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/New Test Data</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>c51f9675-a99a-4655-9345-b00bda1cd4b4</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Username</value>
+         <variableId>7014b36d-8f61-4b40-b7e4-c70197874e54</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>c51f9675-a99a-4655-9345-b00bda1cd4b4</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>11624469-b937-4722-9f9b-6a5f0121d019</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
